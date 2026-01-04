e4ded976cda05dd64e093ef02f418a7d9b3eff3a
Marina <marinamaksimovskaa3@gmail.com>
2026-01-04 14:17:54 +0200

    ff

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,22 +1,15 @@
-import Link from "next/link";
-import css from "./Footer.module.css";
+import css from './Footer.module.css';
 
 export default function Footer() {
   return (
     <footer className={css.footer}>
       <div className={css.content}>
-        <p>
-          © {new Date().getFullYear()} NoteHub. All rights reserved.
-        </p>
-
+        <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
           <p>Developer: Marina</p>
-
           <p>
-            Contact us:{" "}
-            <Link href="mailto:student@notehub.app">
-              student@notehub.app
-            </Link>
+            Contact us:
+            <a href="mailto:student@notehub.app">student@notehub.app</a>
           </p>
         </div>
       </div>
